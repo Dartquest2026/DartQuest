daff7f68e3413ff173c9523005c3a375260a66c9
Dartquest2026 <fotyoner@hotmail.de>
2026-08-05 20:42:52 +0200

    Leveldaten 1-10 erstellt

---
+++ dartquest/src/data/levels.js
@@ -0,0 +1,111 @@
+export const levels = [
+  {
+    id: 1,
+    title: "Level 1",
+    task: "Triff 3x Single 20",
+    minArrows: 3,
+    maxArrows: 9,
+    rewardXP: 20,
+    rewardCoins: 10,
+    boss: false
+  },
+
+  {
+    id: 2,
+    title: "Level 2",
+    task: "Triff 3x Single 19",
+    minArrows: 3,
+    maxArrows: 9,
+    rewardXP: 20,
+    rewardCoins: 10,
+    boss: false
+  },
+
+  {
+    id: 3,
+    title: "Level 3",
+    task: "Triff 3x Single 18",
+    minArrows: 3,
+    maxArrows: 9,
+    rewardXP: 20,
+    rewardCoins: 10,
+    boss: false
+  },
+
+  {
+    id: 4,
+    title: "Level 4",
+    task: "Triff 5x Single 20",
+    minArrows: 5,
+    maxArrows: 9,
+    rewardXP: 30,
+    rewardCoins: 15,
+    boss: false
+  },
+
+  {
+    id: 5,
+    title: "Level 5",
+    task: "Triff 2x Double 20",
+    minArrows: 2,
+    maxArrows: 9,
+    rewardXP: 35,
+    rewardCoins: 20,
+    boss: false
+  },
+
+  {
+    id: 6,
+    title: "Level 6",
+    task: "Triff 6x Single 20",
+    minArrows: 6,
+    maxArrows: 9,
+    rewardXP: 40,
+    rewardCoins: 25,
+    boss: false
+  },
+
+  {
+    id: 7,
+    title: "Level 7",
+    task: "Triff 1x Bull",
+    minArrows: 1,
+    maxArrows: 9,
+    rewardXP: 40,
+    rewardCoins: 25,
+    boss: false
+  },
+
+  {
+    id: 8,
+    title: "Level 8",
+    task: "Triff 2x Triple 20",
+    minArrows: 2,
+    maxArrows: 9,
+    rewardXP: 45,
+    rewardCoins: 30,
+    boss: false
+  },
+
+  {
+    id: 9,
+    title: "Level 9",
+    task: "Checke 40",
+    minArrows: 1,
+    maxArrows: 9,
+    rewardXP: 50,
+    rewardCoins: 35,
+    boss: false
+  },
+
+  {
+    id: 10,
+    title: "BOSS",
+    task: "3x Single 20 → 1x Triple 20 → Checke 40",
+    minArrows: 5,
+    maxArrows: 9,
+    rewardXP: 250,
+    rewardCoins: 200,
+    boss: true
+  }
+]
